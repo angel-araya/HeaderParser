774787f89d177a067e29abe54c7210202f28a4ad
Ángel Araya <al.arayaq@gmail.com>
2017-05-23 16:44:41 -0600

    Remove redirect

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ const express = require('express')
 const app = express()
 app.set('port', process.env.PORT || 8080)
 
-app.get('/whoami', (req, res) => {
+app.get('/', (req, res) => {
   res.send(req.ip)
-})
-
-app.get('*', (req, res) => {
-  res.redirect('/whoami')
 })
 
 app.listen(app.get('port'), () => {
