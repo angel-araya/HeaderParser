f6f6d07a5def6978b4e04d7b88bd9a0a0eeef8e3
Ángel Araya <al.arayaq@gmail.com>
2017-05-23 17:14:48 -0600

    Clean the output
    Returns only language code and OS information

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ app.set('port', process.env.PORT || 8080)
 
 app.get('/', (req, res) => {
   res.json({"ipaddress": req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-            "language": req.headers['accept-language'],
-            "software": req.headers['user-agent']})
+            "language": req.headers['accept-language'].split(',')[0],
+            "software": req.headers['user-agent'].match(/\((.*)\)/)[1]
+          })
 })
 
 app.listen(app.get('port'), () => {
