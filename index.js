b974ad25511ef7531b28472aefcb455dcc2ad568
Ángel Araya <al.arayaq@gmail.com>
2017-05-23 17:00:27 -0600

    req.headers[x-forwarded-for] to account for proxy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.set('port', process.env.PORT || 8080)
 
 app.get('/', (req, res) => {
-  res.json({"ip": req.ip })
+  res.json({"ip": req.headers['x-forwarded-for'] || req.connection.remoteAddress })
 })
 
 app.listen(app.get('port'), () => {
