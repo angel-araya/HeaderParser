275b06696d3aa5eabbecd144a4950e206aab8a27
Ángel Araya <al.arayaq@gmail.com>
2017-05-23 16:40:17 -0600

    Print ip from req.ip

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const express = require('express')
 const app = express()
 app.set('port', process.env.PORT || 8080)
 
+app.get('/whoami', (req, res) => {
+  res.send(req.ip)
+})
+
+app.get('*', (req, res) => {
+  res.redirect('/whoami')
+})
+
 app.listen(app.get('port'), () => {
   console.log('App listening on ', app.get('port'))
 })
